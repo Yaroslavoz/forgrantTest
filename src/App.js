b7e1191864e3ccb550cd18fa7131cf52ae7bc9df
yaroslavoz <yaroslavozv@gmail.com>
2020-07-14 21:49:19 +0300

    success merging

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 import React, { useContext, useState } from 'react';
 import './styles/main.css';
 import './styles/style.css';
 import Card from './components/cardcomponent';
 import { SourceContext } from './SourceContext'
 import { w3cwebsocket as W3CWebSocket } from 'websocket'
-
-const client = new W3CWebSocket('wss://min-api.cryptocompare.com/data/coin/generalinfo?fsyms=ETH,LTC,BTC&tsym=XMR')
-=======
-import React, { useContext } from "react";
-import "./styles/main.css";
-import "./styles/style.css";
-import Card from "./components/cardcomponent";
-import { SourceContext } from "./SourceContext";
->>>>>>> 1fbe7ceb5c7637795423268e809463df199f575a
-
 import { Select } from "./components/Select";
+const client = new W3CWebSocket('wss://min-api.cryptocompare.com/data/coin/generalinfo?fsyms=ETH,LTC,BTC&tsym=XMR')
+
+
 
 function App() {
-<<<<<<< HEAD
   const source = useContext(SourceContext)
   const [sourceArray, setSource] = useState(source)
 
@@ -37,21 +28,11 @@ function App() {
 
   return (
     <>
-      <h1 className='text-white'>This is a great React app</h1>
+      <Select />
       <div className=" container w-3/4 mx-auto grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
       {sourceArray.map((it, index) =>
         <Card key={index} src={it.src} alt={it.name} title={it.name}/>
       )}
-=======
-  const source = useContext(SourceContext);
-  return (
-    <>
-      <Select />
-      <div className=" container w-3/4 mx-auto grid  grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-        {source.map((it, index) => (
-          <Card key={index} src={it.src} alt={it.name} title={it.name} />
-        ))}
->>>>>>> 1fbe7ceb5c7637795423268e809463df199f575a
       </div>
     </>
   );
